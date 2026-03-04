dd9fd722493896518136994e278362a682414bb5
kyuukyayi <kyuukyayi@gmail.com>
2026-03-04 12:38:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,65 +2,57 @@ import streamlit as st
 import pandas as pd
 import joblib
 
-
-
 model = joblib.load("rfiris.pkl")
 
-
-
 st.title("IRIS FLOWER CLASSIFICATION APPLICATION")
-
 st.write("Predict the species of an Iris Flower Using a Random Forest Model")
 
-form = st.form("iris form")
+with st.form("iris_form"):
 
-form.subheader("Enter Flower Measurement")
+    st.subheader("Enter Flower Measurement")
 
-sepal_length = form.number_input(
-	"sepal_length (cm)",
-min_value = 4.0,
-max_value = 8.0,
-value = 5.1
-)
+    sepal_length = st.number_input(
+        "sepal_length (cm)",
+        min_value=4.0,
+        max_value=8.0,
+        value=5.1
+    )
 
-sepal_width = form.number_input(
-	"sepal_width (cm)",
-min_value = 1.0,
-max_value = 7.0,
-value = 5.0
-)
+    sepal_width = st.number_input(
+        "sepal_width (cm)",
+        min_value=2.0,
+        max_value=5.0,
+        value=3.5
+    )
 
-petal_length = form.number_input(
-	"petal_length (cm)",
-min_value = 1.0,
-max_value = 7.0,
-value = 5.0
-)
+    petal_length = st.number_input(
+        "petal_length (cm)",
+        min_value=1.0,
+        max_value=7.0,
+        value=1.4
+    )
 
-petal_width = form.number_input(
-	"petal_width (cm)",
-min_value = 1.0,
-max_value = 2.5,
-value = 0.2
-)
+    petal_width = st.number_input(
+        "petal_width (cm)",
+        min_value=0.1,
+        max_value=2.5,
+        value=0.2
+    )
 
-
-submit_button = form.form_submit_button("Predict")
-
+    submit_button = st.form_submit_button("Predict")
 
 if submit_button:
-	input_data = pd.DataFrame({
-		"sepal_length (cm)": [sepal_length],
-		"sepal_width (cm)" : [sepal_width],
-		"petal_length (cm)" : [petal_length],
-		"petal_width (cm)" : [petal_width] 
-		})
-	prediction = model.predict(input_data)
+    input_data = pd.DataFrame({
+        "sepal length (cm)": [sepal_length],
+        "sepal width (cm)": [sepal_width],
+        "petal length (cm)": [petal_length],
+        "petal width (cm)": [petal_width]
+    })
 
+    prediction = model.predict(input_data)
 
-	st.subheader("Prediction Result")
-	st.success(f"Predicted species: {prediction[0]}")
-	submitted = st.form_submit_button("Submit")
+    st.subheader("Prediction Result")
+    st.success(f"Predicted species: {prediction[0]}")
 
 
 
